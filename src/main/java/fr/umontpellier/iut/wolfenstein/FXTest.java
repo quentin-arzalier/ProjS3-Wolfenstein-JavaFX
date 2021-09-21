1ee835994a0361db13162a5c662fd5aa8a7bf94c
quentin-arzalier <quentin.arzalier@free.fr>
2021-09-21 23:26:22 +0200

    JavaFX c'est broken wtf

--- src/main/java/fr/umontpellier/iut/wolfenstein/FXTest.java
+++ src/main/java/fr/umontpellier/iut/wolfenstein/FXTest.java
@@ -9,7 +9,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -18,16 +17,14 @@ public class FXTest extends Application {
 
     private WritableImage monImage;
     private Scene scene;
-    private GraphicsContext context;
-    private Pane root;
-    private Pane layerPane;
-    private Canvas canvas;
 
 
     private AnimationTimer border;
-    private AnimationTimer game;
+    private AnimationTimer snake;
+    private AnimationTimer wall;
     private int directX = 1;
     private int directY = 0;
+    private int wallSize = 10;
 
 
     public static void main(String[] args) {
@@ -36,30 +33,22 @@ public class FXTest extends Application {
 
     @Override
     public void start(Stage primaryStage){
-        root = new Pane();
-        monImage = new WritableImage(32,32);
-        layerPane = new Pane();
-        canvas = new Canvas(320, 320);
-        context = canvas.getGraphicsContext2D();
-        //context.scale(10, 10);
-        //context.setImageSmoothing(false);
-        layerPane.getChildren().add(canvas);
-        root.getChildren().add(canvas);
-        root.setStyle("-fx-background-color: red");
-        scene = new Scene(root, 320, 320);
+        Pane root = new Pane();
+        monImage = new WritableImage(320,200);
+        ImageView pog = new ImageView(monImage);
+        root.getChildren().add(pog);
+        scene = new Scene(root);
         primaryStage.setTitle("Test JavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
         //makeBorder();
-        //startGame();
-        //addHandlers();
-        context.setFill(Color.BLUE);
-        context.fillRect(0, 0, 160, 160);
+        //startSnake();
+        startWall();
+        addHandlers();
     }
 
     private void changePixel(int x, int y, Color c){
         monImage.getPixelWriter().setColor(x, y, c);
-        context.drawImage(monImage, 0, 0);
     }
 
     private void makeBorder() {
@@ -87,15 +76,17 @@ public class FXTest extends Application {
         border.start();
     }
 
-    private void startGame(){
-        game = new AnimationTimer() {
+    private void startSnake(){
+        snake = new AnimationTimer() {
             private final int startx = 2;
             private final int starty = 2;
             private int x = startx;
             private int y = starty;
             private long lastUpdate;
+            private long test;
             @Override
             public void handle(long now) {
+                System.out.println("Depuis la frame précédente, " + ((now - test) / 1_000_000) + "ms se sont écoulées.");
                 if (now - lastUpdate >= 50_000_000) {
                     changePixel(x,y, Color.WHITE);
                     x+=directX;
@@ -108,9 +99,35 @@ public class FXTest extends Application {
                     }
                     lastUpdate = now;
                 }
+                test = now;
             }
         };
-        game.start();
+        snake.start();
+    }
+
+    private void startWall(){
+        wall = new AnimationTimer() {
+
+            private long lastUpdate = 0;
+            @Override
+            public void handle(long now) {
+                //System.out.println("Depuis la frame précédente, " + ((now - lastUpdate) / 1_000_000) + "ms se sont écoulées.");
+                System.out.println("fps = " + 1_000/((now-lastUpdate) / 1_000_000));
+                for (int x = 0; x < 320; x++) {
+                    for (int y = 0; y < 200; y++) {
+                        if (y < 100 - wallSize/2) {
+                            changePixel(x, y, Color.GRAY);
+                        } else if (y < 100 + wallSize/2 -1) {
+                            changePixel(x, y, Color.CADETBLUE);
+                        } else {
+                            changePixel(x, y, Color.DARKGRAY);
+                        }
+                    }
+                }
+                lastUpdate = now;
+            }
+        };
+        wall.start();
     }
 
     private void addHandlers(){
@@ -122,13 +139,20 @@ public class FXTest extends Application {
                 directX = 1;
                 directY = 0;
             } else if (key.getCode()== KeyCode.UP) {
+                if (wallSize < 200){
+                    wallSize++;
+                }
                 directX = 0;
                 directY = -1;
             } else if (key.getCode()== KeyCode.DOWN) {
+                if (wallSize > 0){
+                    wallSize--;
+                }
                 directX = 0;
                 directY = 1;
             } else if (key.getCode()== KeyCode.ESCAPE){
-                game.stop();
+                snake.stop();
+                wall.stop();
             }
         });
     }
