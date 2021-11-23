c5a5f676b0eff9206532b2cc7b392a69fac547c6
quentin-arzalier <quentin.arzalier@free.fr>
2021-11-23 13:09:31 +0100

    Tout plein les tests

---
+++ src/main/test/test/fr/umontpellier/iut/wolfenstein/graphismes/SpriteTest.java
@@ -0,0 +1,17 @@
+package test.fr.umontpellier.iut.wolfenstein.graphismes;
+
+import fr.umontpellier.iut.wolfenstein.TestApp;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SpriteTest {
+
+    @BeforeAll
+    static void setUp(){
+        TestApp.main(new String[0]);
+    }
+
+    @Test
+    void SpriteDist() {
+    }
+}
